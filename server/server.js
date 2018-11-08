2acda46d0302cd15491107b921b87a44207cb452
John Pawlak <johnpawlak@gmail.com>
2018-11-08 08:46:31 -0500

    initalize node chat app with socketio

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const express = require('express')
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000
+var app = express();
+
+app.use(express.static(publicPath))
+
+// app.get('/', (req, res) => {
+//     res.send()
+// })
+
+
+
+// console.log(__dirname + '/../public');
+// console.log(publicPath);
+
+app.listen(port, () => {
+    console.log(`server on port ${port}`)
+})
