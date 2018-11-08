3bd5ae1100f4043155fae505dbe848a504a380fa
John Pawlak <johnpawlak@gmail.com>
2018-11-08 14:12:49 -0500

    added socket.io library, script and initial connection

--- server/server.js
+++ server/server.js
@@ -1,21 +1,23 @@
-const path = require('path')
-const express = require('express')
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000
 var app = express();
+var server = http.createServer(app)
+//pass server that we want to use with our websockets (how to communicate with server and client)
+var io = socketIO(server)
 
+//middleware for pathing
 app.use(express.static(publicPath))
 
-// app.get('/', (req, res) => {
-//     res.send()
-// })
+//register an event listener, listen for event and do something when it happens
+io.on('connection', (socket) => {
+    console.log('new user connected')
+})
 
-
-
-// console.log(__dirname + '/../public');
-// console.log(publicPath);
-
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`server on port ${port}`)
 })
