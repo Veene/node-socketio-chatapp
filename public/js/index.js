18a78485ad22f05566156f69a2dccbe78bea4683
John Pawlak <johnpawlak@gmail.com>
2018-11-28 21:42:06 -0500

    Emit newMessage on createMEssage

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,13 @@
-//io is a method that initiates a request from client to server to open up a websocket and keep it open - make sure to save in variable 'socket'
 var socket = io();
-        
-//first argument is event name, second is callback function
-socket.on('connect', function() {
-    console.log('connected to server')
 
-    socket.emit('createMessage', {
-        from: 'veene',
-        text: 'hey everyone'
-    })
-})
-socket.on('disconnect', function() {
-    console.log('disconnected from server')
-})
-socket.on('newEmail', function(email) {
-    console.log('new email', email);
-})
-socket.on('newMessage', function(msg) {
-    console.log('client message: ', msg.clientMessage)
-    console.log('createdAt: ', msg.createdAt)
-})
+socket.on('connect', function () {
+  console.log('Connected to server');
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+});
--- server/server.js
+++ server/server.js
@@ -4,42 +4,47 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 var app = express();
-var server = http.createServer(app)
-//pass server that we want to use with our websockets (how to communicate with server and client)
-var io = socketIO(server)
+var server = http.createServer(app);
+var io = socketIO(server);
 
-//middleware for pathing
-app.use(express.static(publicPath))
+app.use(express.static(publicPath));
 
-//register an event listener, listen for event and do something when it happens
 io.on('connection', (socket) => {
-    console.log('new user connected')
+  console.log('New user connected');
 
-    socket.emit('newEmail', {
-        from: 'mike@example.com',
-        text: 'Hey. Whats going on?',
-        createdAt: 123
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
     });
-    socket.on('createEmail', (newEmail) => {
-        console.log('createEmail: ', newEmail)
-    })
-    socket.on('createMessage', function(message) {
-        console.log('createMessageFromClient: ', message)
-        var clientMessage = `Hi everyone, ${message.from} wants to say: ${message.text}`
-        socket.emit('newMessage', {
-            clientMessage : clientMessage,
-            createdAt: 123123
-        })
-    })
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+  });
 
-    socket.on('disconnect', () => {
-        console.log('user was disconnected')
-    })
-})
-
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
 
 server.listen(port, () => {
-    console.log(`server on port ${port}`)
-})
+  console.log(`Server is up on ${port}`);
+});
